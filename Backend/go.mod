6f9b3be704ed197d049da69805c9e790aca945db
Shaked Govrin <shakedgo@gmail.com>
2024-05-08 21:19:18 +0300

    Modified graceful shutdown

--- Backend/go.mod
+++ Backend/go.mod
@@ -3,6 +3,7 @@ module Go-Poker
 go 1.22.1
 
 require (
+	github.com/gin-contrib/cors v1.7.2
 	github.com/gin-gonic/gin v1.9.1
 	github.com/joho/godotenv v1.5.1
 	go.mongodb.org/mongo-driver v1.15.0
@@ -10,14 +11,15 @@ require (
 )
 
 require (
-	github.com/bytedance/sonic v1.11.3 // indirect
-	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
-	github.com/chenzhuoyu/iasm v0.9.1 // indirect
+	github.com/bytedance/sonic v1.11.6 // indirect
+	github.com/bytedance/sonic/loader v0.1.1 // indirect
+	github.com/cloudwego/base64x v0.1.4 // indirect
+	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.19.0 // indirect
+	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
@@ -43,7 +45,6 @@ require (
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/protobuf v1.33.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	google.golang.org/protobuf v1.34.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- Backend/main.go
+++ Backend/main.go
@@ -6,6 +6,11 @@ import (
 	"Go-Poker/pkg/utils"
 	"context"
 	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -13,11 +18,52 @@ func main() {
 	api.StartRouter()
 	utils.InitTable()
 
-	// Close the database connection when the application exits
-	defer func() {
-		if err := db.Client.Disconnect(context.TODO()); err != nil {
-			panic(err)
+	// Use a WaitGroup to coordinate between the database and server shutdown
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, syscall.SIGINT, syscall.SIGTERM)
+
+	shutdownComplete := make(chan struct{})
+
+	// Handle shutdown for database
+	go func() {
+		defer wg.Done()
+
+		<-stopChan // Wait for SIGINT or another signal
+
+		log.Println("Shutdown Database ...")
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := db.Client.Disconnect(ctx); err != nil {
+			log.Fatal("Database Shutdown:", err)
+		}
+		if ctx.Err() == context.DeadlineExceeded {
+			log.Println("Timeout of 5 seconds.")
 		}
 		log.Println("Disconnected Database")
+
+		log.Println("Shutdown HTTP Server ...")
+		ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := api.Server.Shutdown(ctx); err != nil {
+			log.Fatal("HTTP Server Shutdown:", err)
+		}
+		if ctx.Err() == context.DeadlineExceeded {
+			log.Println("Timeout of 5 seconds.")
+		}
+		log.Println("HTTP Server shutdown complete.")
+		close(shutdownComplete)
 	}()
+
+	// Wait for both shutdowns to complete
+	go func() {
+		wg.Wait()
+		log.Println("All shutdowns complete.")
+	}()
+
+	// Wait for the shutdown to complete
+	<-shutdownComplete
+	log.Println("All shutdowns complete.")
 }
