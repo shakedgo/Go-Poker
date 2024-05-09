5058f9d1a651309a6c02f3279e4c87f1e8690199
Shaked Govrin <shakedgo@gmail.com>
2024-05-09 20:49:05 +0300

    Fixed graceful shutdown

--- Backend/pkg/api/router.go
+++ Backend/pkg/api/router.go
@@ -1,16 +1,16 @@
 package api
 
 import (
-	"context"
 	"log"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
 	"github.com/gin-gonic/gin"
 )
+
+var Server = &http.Server{
+	Addr:    ":8080",
+	Handler: gin.Default(),
+}
 
 func StartRouter() {
 	router := gin.Default()
@@ -26,33 +26,11 @@ func StartRouter() {
 	router.GET("/print-table/:id", PrintTable)
 	router.GET("/print-player/:id", PrintPlayer)
 
-	srv := &http.Server{
-		Addr:    ":8080",
-		Handler: router,
-	}
+	Server.Handler = router
 
 	go func() {
-		// service connections
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		if err := Server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("listen: %s\n", err)
 		}
 	}()
-
-	// Wait for interrupt signal to gracefully shutdown the server with
-	// a timeout of 5 seconds.
-	var stopChan = make(chan os.Signal, 1)
-	signal.Notify(stopChan, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
-
-	<-stopChan // wait for SIGINT
-	log.Println("Shutdown Server ...")
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatal("Server Shutdown:", err)
-	}
-	if ctx.Err() == context.DeadlineExceeded {
-		log.Println("Timeout of 5 seconds.")
-	}
-	log.Println("Server exiting")
 }
